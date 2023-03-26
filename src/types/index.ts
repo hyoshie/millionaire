1b9999524a65af3e43b78036c885e5cbc090f723
Hiroki Yoshie <dymn9.term@gmail.com>
2023-03-26 12:02:48 +0900

    fix: Question型を新しいデータベースに対応させた

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,26 @@
 export type Question = {
   id: number;
-  question_text: string;
-  difficulty_level: number;
   category_id: number;
+  question: string;
+  option_a: string;
+  option_b: string;
+  option_c: string;
+  option_d: string;
+  correct_option: CorrectOption;
+  difficulty: Difficulty;
   created_at: Date;
   updated_at: Date;
 };
+
+export enum CorrectOption {
+  A = 'a',
+  B = 'b',
+  C = 'c',
+  D = 'd',
+}
+
+export enum Difficulty {
+  Easy = 'easy',
+  Hard = 'hard',
+  Medium = 'medium',
+}
